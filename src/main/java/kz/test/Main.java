819dc5e5d9caa45ea6bee80756d83c22326dce09
Anvar HunT <anvar.hunt@gmail.com>
2021-02-20 21:42:49 +0600

    Practice2 tasks

--- src/main/java/kz/test/Main.java
+++ src/main/java/kz/test/Main.java
@@ -1,8 +1,59 @@
 package kz.test;
 
+import kz.test.practice2.Practice2;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] arr;
 
+        //1
+        arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0};
+        printInitialArray(arr);
+        Practice2.swapSymbolsInArray(arr);
+
+        //2
+        arr = new int[7];
+        printInitialArray(arr);
+        Practice2.initializeArray(arr);
+
+        //3
+        arr = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        printInitialArray(arr);
+        Practice2.multiplySmallValues(arr);
+
+        //4
+        int[][] arr4 = new int[5][5];
+        Practice2.fillDiagonalElements(arr4);
+
+        //5
+        arr = new int[getRandomNumber(5,10)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = getRandomNumber(-100, 100);
+        }
+        printInitialArray(arr);
+        Practice2.findOutliers(arr);
+
+        //6
+        arr = new int[] {2, 2, 2, 1, 2, 2, 10, 1};
+        System.out.println("Check on balance: " + Practice2.checkOnPossibleBalance(arr));
+
+        //7
+        arr = new int[] {1,2,3,4,5};
+        printInitialArray(arr);
+        Practice2.moveArray(arr, 3);
+    }
+
+
+    public static void printInitialArray(int[] arr) {
+        System.out.println("Initial array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
--- src/main/java/kz/test/practice2/Practice2.java
+++ src/main/java/kz/test/practice2/Practice2.java
@@ -1,5 +1,115 @@
 package kz.test.practice2;
 
+import java.util.Arrays;
+
 public class Practice2 {
 
+    //1
+    public static void swapSymbolsInArray(int[] arr) {
+        System.out.println("Start printing array : ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //2
+    public static void initializeArray(int[] arr) {
+        System.out.println("Start printing array : ");
+        int bias = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = bias;
+            bias += 3;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //3
+    public static void multiplySmallValues(int[] arr) {
+        System.out.println("Start printing array : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] < 6 ? arr[i] * 2 : arr[i];
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //4
+    public static void fillDiagonalElements(int[][] arr) {
+        System.out.println("Start printing array : ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == j || j + i == arr.length - 1) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //5
+    public static void findOutliers(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max ) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Min value: " + min + "\nMax value: " + max);
+    }
+
+    //6
+    public static boolean checkOnPossibleBalance(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int[] subArr1 = Arrays.copyOfRange(arr, 0, i + 1);
+            int[] subArr2 = Arrays.copyOfRange(arr, i + 1, arr.length);
+            if (sumOfElements(subArr1) == sumOfElements(subArr2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private static int sumOfElements(int[] subArr) {
+        return Arrays.stream(subArr).sum();
+    }
+
+    //7
+    public static void moveArray(int[] arr, int offset) {
+        boolean isRightDirection = offset > 0;
+        offset = Math.abs(offset);
+        for (int j = 0; j < offset; j++) {
+            if (isRightDirection) {
+                int last = arr[arr.length - 1];
+                for (int i = arr.length - 2; i >= 0; i--) {
+                    arr[i+1] = arr[i];
+                }
+                arr[0] = last;
+            } else {
+                int first = arr[0];
+                for (int i = 1; i < arr.length; i++) {
+                    arr[i-1] = arr[i];
+                }
+                arr[arr.length - 1] = first;
+            }
+        }
+
+        System.out.println("Moved array edition: ");
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
 }
