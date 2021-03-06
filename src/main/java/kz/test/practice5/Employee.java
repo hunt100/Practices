ed3839d86ce311dafaba68b94f39209772a9fbc0
Anvar HunT <anvar.hunt@gmail.com>
2021-03-07 01:28:54 +0600

    Practice 5 OOP basics

---
+++ src/main/java/kz/test/practice5/Employee.java
@@ -0,0 +1,87 @@
+package kz.test.practice5;
+
+import java.math.BigDecimal;
+
+public class Employee {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private BigDecimal salary;
+    private int age;
+
+    public Employee(String fullName,
+                    String position,
+                    String email,
+                    String phone,
+                    BigDecimal salary,
+                    int age) {
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.position = position;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
